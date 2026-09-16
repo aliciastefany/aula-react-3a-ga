3484277fa7c756d93a1683bb1a4e4a01e81f706b
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-09-16 11:10:59 -0300

    navegação finalizada

---
+++ aula-react/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h2>Página Inicial</h2>
+            <p>Seja bem-vindo(a) ao meu projeto!</p>
+        </div>
+    )
+}
+
+export default Home
