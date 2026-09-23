71d85cbc5a4cd78509ac87b64cd24f0de9d21dec
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-09-23 10:25:01 -0300

    projeto final

--- aula-react/src/App.jsx
+++ aula-react/src/App.jsx
@@ -1,30 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import {Link, Routes, Route} from 'react-router'
+
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Contato from './pages/Contato'
 
 function App() {
-  const [pagina, setPagina] = useState('home')
-
-  const renderizarPagina= ()=>{
-    if (pagina === 'home') return <Home/>
-    if (pagina === 'sobre') return <Sobre/>
-    if (pagina === 'contato') return <Contato/>
-    
-  }
-
   return (
     <div id="container">
       <h1>Sejam bem-vindos a aula de React!</h1>
       <nav>
-        <button onClick={()=>setPagina('home')}>Home</button>
-        <button onClick={()=>setPagina('sobre')}>Sobre</button>
-        <button onClick={()=>setPagina('contato')}>Contato</button>
+        <Link to='/'>Home</Link>
+        <Link to='/sobre'>Sobre</Link>
+        <Link to='/contato'>Contato</Link>
       </nav>
-      <div className='conteudo'>
-        {renderizarPagina()}
-      </div>
+      <hr/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+      </Routes>
     </div>
   )
 }
